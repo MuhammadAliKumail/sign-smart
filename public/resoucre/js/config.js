c2f12dfc4e7ab697505d9538d24d0c45edf5a43d
MuhammadAliKumail <mohammadalikumail@gmail.com>
2024-07-16 23:35:29 +0500

    update

---
+++ public/resoucre/js/config.js
@@ -0,0 +1,14 @@
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBmLHlHvokjkwjuQHhhG6CD8rnYZsjebEg",
+  authDomain: "sign-smart-35f1e.firebaseapp.com",
+  projectId: "sign-smart-35f1e",
+  storageBucket: "sign-smart-35f1e.appspot.com",
+  messagingSenderId: "531178023291",
+  appId: "1:531178023291:web:b67911b4d8c13f0270b5de",
+  measurementId: "G-X77XV9BPR3",
+  databaseURL: "https://sign-smart-35f1e-default-rtdb.firebaseio.com/",
+};
+
+// module.exports = firebaseConfig;
+export default firebaseConfig;
